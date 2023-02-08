c61afff4eb7ad333e142496531a1e27bccacb40c
rainswerld <m.rainrobinson@gmail.com>
2023-02-07 20:48:20 -0700

    Update notes

--- guessing_game.rb
+++ guessing_game.rb
@@ -4,11 +4,24 @@
 
 secret_word = "frugal"
 guess = ""
+guess_count = 0
+guess_limit = 3
+out_of_guesses = false
 
 # while loop asking the user to guess
-while guess != secret_word
-    puts "Enter your guess: "
-    guess = gets.chomp()
+while guess != secret_word and !out_of_guesses
+    if guess_count < guess_limit
+        puts "Enter your guess: "
+        guess = gets.chomp()
+        guess_count += 1
+    else
+        out_of_guesses = true
+    end
+    
 end
 
-puts "The word is indeed " + secret_word
+if out_of_guesses
+    puts "You lose"
+else
+    puts "You won"
+end
